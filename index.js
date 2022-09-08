f835fa68efc8b6d66f0715869c91539315e5e0ae
adjasnie <adjasnie@adjasnies-MacBook-Pro.local>
2022-09-08 14:57:46 -0400

    trying with cisco vmanage (no login)

--- index.js
+++ index.js
@@ -10,13 +10,12 @@ const main = () => {
     Math.ceil(original_collection.item.length / 4)
   );
 
-  if(parseInt(process.env.CIRCLE_NODE_INDEX) > collections.length - 1){
-    console.log("No need for this runner")
-    process.exit(0)
+  if (parseInt(process.env.CIRCLE_NODE_INDEX) > collections.length - 1) {
+    console.log("No need for this runner");
+    process.exit(0);
   }
 
-  console.log(`RUNNER ${process.env.CIRCLE_NODE_INDEX} ACTIVE`)
-
+  console.log(`RUNNER ${process.env.CIRCLE_NODE_INDEX} ACTIVE`);
 
   newman
     .run({
@@ -25,7 +24,15 @@ const main = () => {
         info: original_collection.info,
         item: collections[process.env.CIRCLE_NODE_INDEX],
       },
+      insecure: true,
       reporter: ["json-summary"],
+      envVar: require("./Test.postman_environment.json").values.map((el) => {
+        return {
+          "key": el.key,
+          "value": process.env[key.toUpperCase()] || el.value
+        }
+      }),
+      environment: require("./Test.postman_environment.json"),
     })
     .on("done", function (err, summary) {
       if (err !== null) {
